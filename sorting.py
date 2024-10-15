a71ef8cc64941e5eaa2cd85238f865046277dc62
Clarskelis2 <153529305+Clarskelis2@users.noreply.github.com>
2024-10-15 13:32:29 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,5 +1,22 @@
 #bubble(list) - > sorted ist
 
-#selection(list) -> sorted list
+def selection(list):
+  for i in range(len(list)-1):
+    index_smallest = i
+    for j in range(i+1, len(list)):
+      if list[j] < list[index_smallest]:
+        index_smallest = j
+    temp = list[i]
+    list[i] = list[index_smallest]
+    list[index_smallest] = temp
+  return list
 
-#insertion(list) > sorted list
+def insertion(list):
+  for i in range(len(list)):
+    j = i
+    while (j > 0 && list[j] < list[j - 1]):
+      temp = list[j]
+      list[j] = list[j - 1]
+      list[j - 1] = temp
+      j -= 1
+  return list
